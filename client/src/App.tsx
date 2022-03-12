ecdd46e865e451756ee0594e397d447a821a5900
Miraaj Kadam <miraajkadam@gmail.com>
2022-03-12 17:04:28 +0530

    Improve graphql error displays

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,24 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import { ApolloClient, ApolloProvider, from, HttpLink, InMemoryCache } from "@apollo/client";
+import { onError } from "@apollo/client/link/error";
 import { FC, Fragment } from "react";
 import AddBook from "./components/AddBook";
 import BookList from "./components/BookList";
 
-const client = new ApolloClient({
+const httpLink = new HttpLink({
   uri: "http://localhost:5000/graphql",
+});
+
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({ message, locations, path }) =>
+      console.log(`[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`)
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const client = new ApolloClient({
+  link: from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
 
